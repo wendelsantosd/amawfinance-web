ba5bf8db452d780ce53b200f43e0be6e2d5a2fa3
WendelSantosD <wendelwcsantos@gmail.com>
2022-02-24 23:00:46 -0300

    Init menu.

---
+++ src/Components/Menu/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Container } from './menu.styles'
+
+export const Menu = () => {
+    return <Container>
+        <p>Hello World !</p>
+    </Container>
+}
--- src/Pages/Profile/index.tsx
+++ src/Pages/Profile/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import { Header } from '../../Components/Header'
+import { Menu } from '../../Components/Menu'
 import { Container } from './profile.styles'
 
 export const Profile = () => {
     return <Container>
         <Header isAuth/>
+        <Menu />
     </Container>
 }
