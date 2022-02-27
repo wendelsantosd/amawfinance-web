847cba02b8030805b01d6bf8149132716d28ab07
WendelSantosD <wendelwcsantos@gmail.com>
2022-02-27 10:59:53 -0300

    Implements storage service.

---
+++ src/services/storage.ts
@@ -0,0 +1,26 @@
+export default {
+    write: (key: string, value: { [key: string]: any }): void => {
+        localStorage.setItem(
+            key,
+            JSON.stringify(value)
+        )
+    },
+
+    read: (key: string): any | null => {
+        const value = localStorage.getItem(key)
+
+        if (value) {
+            return JSON.parse(value)
+        } else {
+            return null
+        }
+    },
+
+    clear: (item: string): void => {
+        if (item === 'all') {
+            localStorage.clear()
+        } else {
+            localStorage.removeItem(item)
+        }
+    }
+}
