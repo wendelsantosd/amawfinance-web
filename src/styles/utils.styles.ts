a0dc6a8b0d2371f8f9d9e242f5cfff73e41b5e2a
wendelsantostrizy <wendel.santos@trizy.com.br>
2022-03-09 22:17:05 -0300

    Modify color password to gray.

--- src/styles/utils.styles.ts
+++ src/styles/utils.styles.ts
@@ -101,6 +101,7 @@ export const ContainerPasswordInput = styled.div`
 
     .input-password {
         position: absolute;
+        color: var(--grey-300);
         width: 100%;
         height: 100%;
         border: none;
