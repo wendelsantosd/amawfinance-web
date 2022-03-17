429b41fe3ccc73d381ffa45ea3d3b290af35f448
WendelSantosD <wendelwcsantos@gmail.com>
2022-03-16 21:09:20 -0300

    implements integration test  to page login.

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -1,8 +1,32 @@
-describe('Page Login', () => {
-    it('should be redirect to register page', () => {
-        cy.visit('http://localhost:3000/login')
+describe('Login Page', () => {
 
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/login')
+    })
+
+    it('should be redirect to register page', () => {
         cy.get('#redirect-register').click()
         cy.url().should('eq', 'http://localhost:3000/register')
+    })
+
+    it('should be authenticate and redirect to page transactions' , () => {
+        cy.get('#email').type('admin@provider.com')
+            .should('have.value', 'admin@provider.com')
+        
+        cy.get('#password').type('admin')
+            .should('have.value', 'admin')
+
+        cy.get('#btn-auth').click()
+
+        cy.url().should('eq', 'http://localhost:3000/transactions')
+    })
+
+    it('should be redirect to recover password page', () => {
+        cy.get('#redirect-recover-password').click()
+        cy.url().should('eq', 'http://localhost:3000/recover-password')
+    })
+
+    it('should be click in google button', () => {
+        cy.get('#btn-google-auth').click()
     })
 })
